79ae95c4ec02560f910ab83737f11d70774afb9d
Rodrigo Xavier Vargas <90809287+RodrigoXVargas@users.noreply.github.com>
2025-03-13 10:02:12 -0300

    Update CreateAdmin.java

--- src/main/java/com/e_music/project_emusic/config/CreateAdmin.java
+++ src/main/java/com/e_music/project_emusic/config/CreateAdmin.java
@@ -31,45 +31,7 @@ public class CreateAdmin implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        /*User user = new User();
-        String passwordEncoded = passwordEncoder.encode("evelin");
-        user.setPassword(passwordEncoded);
-        user.setEmail("evelin@gmail.com");
-        user.setDni(12312312);
-        user.setName("Evelin");
-        user.setLastName("Ortiz");
-        //Rol rolAdmin = serviceRol.getByRolName(RolName.ROLE_ADMIN).get();
-        Rol rolUser = serviceRol.getByRolName(RolName.ROLE_USER).get();
-        Set<Rol> rols = new HashSet<>();
-        //rols.add(rolAdmin);
-        rols.add(rolUser);
-        user.setRoles(rols);
-        serviceUser.saveOne(user);*/
-
-        /*User user = serviceUser.findById(2L);
-        log.info(user.getEmail());
-        Address address = serviceAddress.findById(4L);
-        log.info(address.getStreet());
-        log.info(String.valueOf(address.getNumber()));
-        user.setAddress(address);
-        user.setDni(40123123);
-        user.setName("Alejo");
-        user.setLastName("Araya");
-        log.info(String.valueOf(user.getAddress().getId()));
-        serviceUser.updateOne(user, 2L);*/
-
-        //user.setAddress(address);
-        //serviceUser.updateOne(user, 2L);
-
-        /*List<Address> addresses = serviceAddress.findAll();
-        for (Address address: addresses) {
-            log.info(address.getStreet());
-            log.info(String.valueOf(address.getNumber()));
-            log.info(address.getLocation());
-            log.info(address.getProvince());
-            log.info(String.valueOf(address.getPostalCode()));
-            log.info("---------------------------------------");
-        }*/
+       
 
     }
 }
